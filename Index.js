cb5bf22f0e65f320fae19f04b1c4c2120e207621
miriam <alexandratache06@gmail.com>
2022-10-27 13:29:04 +0300

    submit-button-ready

--- Index.js
+++ Index.js
@@ -1,13 +1,22 @@
+var inputText = document.querySelector(".the-new-task");
+inputText.addEventListener("keypress", function (submit) {
+  if (submit.key === "Enter") {
+    submit.preventDefault();
+    document.getElementById("add-tsk-btn").click();
+  }
+});
+
 function addTask() {
   const div = document.createElement("div");
 
   div.className = "confirmed-task";
 
-  div.innerHTML = `
-    <div class="container">
-      <div class="task-name">Fa-ti patul</div>
-      <div class="task-hour">8:00</div>
-    </div>
-    `;
+  div.innerHTML =
+    inputText.value +
+    ` <button class="edit-btn btn ">
+  <i class="bi bi-pencil-square"></i>
+</button>`;
   document.getElementById("task-container").appendChild(div);
+
+  inputText.value = "";
 }
